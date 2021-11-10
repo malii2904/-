9ab0738b251ebc7c8cb246b1db9802f5ddcf0549
Марія Малій <Mariia.Malii@cs.khpi.edu.ua>
2021-11-10 16:44:23 +0300

    done

---
+++ REPOS/Practice_1/№5/№5.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 2, y, z;
+
+	x *= 3 + 2;
+	cout << x << endl;
+
+	x *= y = z = 4;
+	cout << x << endl << y << endl << z << endl;
+
+	x = y == z;
+	cout << x << endl << y << endl << z << endl;
+
+
+	x = x == (y = z);
+	cout << x << endl << y << endl << z << endl;
+}
+
