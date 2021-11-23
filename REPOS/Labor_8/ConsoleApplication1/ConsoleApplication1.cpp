35796192250d42f2b9cbd4f1f213c4bdc95234d1
Марія Малій <Mariia.Malii@cs.khpi.edu.ua>
2021-11-23 20:41:41 +0300

    done

---
+++ REPOS/Labor_8/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,50 @@
+﻿
+/* #include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	int N = 12, a(0);// N - количество элементов
+	int A[12];
+	for (int i = 0; i < N; i++) cin >> A[i];//заполнение масива
+	for (int i = 0; i < 100; i++) //сам переворот массива
+	{
+		a = A[i];
+		A[i] = A[N - i - 1];
+		A[N - i - 1] = a;
+	}
+	for (int i = 0; i < N; i++) cout << A[i] << "; ";
+	
+	return 0;
+ */
+
+
+
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int mass[100];
+	cout << "Начальный массив:" << endl;
+	for (int i = 0; i < 100; i++) {
+		mass[i] = rand() % 100 - 50;
+		cout <<setw(5)<< mass[i] <<""<< endl;
+	}
+
+
+
+
+
+
+}
+
+   
+        
---
+++ REPOS/Labor_8/GGGG/GGGG.cpp
@@ -0,0 +1,39 @@
+﻿ #include <iostream>
+using namespace std;
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <iomanip>
+#include <algorithm>
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int N = 100, k, t;
+    //int mass[100];
+    int* mass = new int[100 + 1];
+    cout << "Начальный массив:" << endl;
+    for (int i = 0; i < 100; i++) {
+        mass[i] = rand() % 100 - 50;
+        cout << setw(5) << mass[i] << " ";
+    }
+    mass[100] = -1;
+    k = t = 0;
+    for (int i = t; i < 100 + 1; i++) {
+        if ((i + 1) < 100 + 1 && mass[i] >= 0) {
+            k++;
+        }
+        else {
+            reverse(mass + i - k, mass + i);
+            t = k;
+            k = 0;
+        }
+    }
+    cout << "Массив-результат:" << endl;
+    for (int i = 0; i < N; i++) cout << setw(5) << mass[i] << ";";
+    return 0;
+}
+
+
+
+
+
