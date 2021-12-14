d6490967171c3f295c5d8d09c5a79c34c2169e54
Марія Малій <Mariia.Malii@cs.khpi.edu.ua>
2021-12-15 01:34:44 +0300

    done

---
+++ REPOS/Labor_6/Labor_6a/Labor_6a.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <time.h>
+#include <iostream>
+#pragma warning(disable : 4996)
+
+int main(void)
+{
+    setlocale(LC_ALL, "Rus");
+    srand(time(0));
+    int randnum = rand() % 100;
+    int randnum2 = 0;
+    int k = 0;
+    printf("Отгадайте число от 0 до 100\n");
+    while (randnum2 != randnum)
+    {
+        ++k;
+        printf("Введите ваше значениe ");
+        scanf("%d", &randnum2);
+        if (randnum2 > randnum)
+            printf("Число больше загаданного компьюеторм\n");
+        else if (randnum2 < randnum)
+            printf("Число меньше загаданного компьюеторм\n");
+    }
+    printf("Вы угадали за"" %d ""попыток\n", k);
+    printf("\n\n\n");
+    return 0;
+
+
+}
---
+++ REPOS/Labor_6/Labor_6b/Labor_6b.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+#include <stdio.h>
+#include <ctime>
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    srand(time(NULL));
+    int a = rand() % 10;
+    int b = rand() % 10;
+    int d;// ответ пользователя
+    int f = 0;// кол-во верных ответов
+    int exs = 10;
+
+    for (int i = 0; i < exs; ++i) {
+        a = rand() % 10;
+        b = rand() % 10;
+        d = a * b;
+        int n = a * b;
+        cout << "a=" << a << "b=" << b << endl;
+        cout << "a*b=";
+        cin >> d;
+        if (d == n) {
+            f++;
+        }
+        else (d != n);
+        {
+            f += 0;
+        }
+    }
+    cout << "Количество верных ответов=" << f << endl;
+    int g = (f * 12) / exs;
+    cout << "Ваша оценка:" << g;
+
+
+
+}
