e9d7344df83c2356b8bce037166a7f9693590368
Марія Малій <Mariia.Malii@cs.khpi.edu.ua>
2021-12-15 01:18:40 +0300

    done

---
+++ REPOS/Labor_5/Labor_5/Labor_5.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+#include <stdio.h>
+#pragma warning (disable:4996)
+
+int main(void)
+{
+    setlocale(LC_ALL, "Rus");
+    double x, y;
+    printf("Введите координату x >");
+    scanf("%lf", &x);
+    printf("Введите координату y >");
+    scanf("%lf", &y);
+    printf("x=%6.3lf;  y=%6.3lf\n", x, y);
+
+    if ((y > x * x - 1))
+    {
+        (0 >= x <= 0) && (y <= 1) && (y >= 1);
+        printf("Точка попадает в область\n");
+    }
+
+    else
+        printf("Точка не попадает в область\n");
+
+    return 0;
+
+}
---
+++ REPOS/Labor_5/Labor_5a/Labor_5a.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#pragma warning (disable:4996)
+
+
+int main(void)
+{
+    setlocale(LC_ALL, "Rus");
+    float a, b, c;
+    float  x1, x2;
+    float D;
+
+    printf("Введите значение а= ");
+    scanf("%f", &a);
+
+    printf("Введите значение b= ");
+    scanf("%f", &b);
+
+    printf("Введите значение c= ");
+    scanf("%f", &c);
+
+    D = b * b - 4 * a * c;
+
+
+    if (D >= 0)
+    {
+        x1 = (-b + sqrt(D)) / (2 * a);
+        x2 = (-b - sqrt(D)) / (2 * a);
+        printf("x1=%f\n", x1);
+        printf("x2=%f\n", x2);
+    }
+    if (D < 0)
+        printf("Корней не существует");
+
+}
